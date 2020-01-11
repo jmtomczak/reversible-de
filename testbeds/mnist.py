09e9159115b1cbece27a74d40e29c4cdc7a3be62
jmtomczak <jakubmkt@gmail.com>
2020-01-11 21:27:31 +0100

    Downloading mnist

---
+++ testbeds/mnist.py
@@ -0,0 +1,41 @@
+import os
+import gzip
+import pickle
+import numpy as np
+from urllib import request
+
+# PYTHONPATH = '/home/jakub/Dev/github/life'  # ripper5
+PYTHONPATH = '/Users/jmt/Dev/github/life'  #mac
+
+filename = [
+["training_images","train-images-idx3-ubyte.gz"],
+["test_images","t10k-images-idx3-ubyte.gz"],
+["training_labels","train-labels-idx1-ubyte.gz"],
+["test_labels","t10k-labels-idx1-ubyte.gz"]
+]
+
+
+def download_mnist(location):
+    base_url = "http://yann.lecun.com/exdb/mnist/"
+    for name in filename:
+        print("Downloading " + name[1] + "...")
+        request.urlretrieve(base_url + name[1], location + name[1])
+    print("Download complete.")
+
+
+def save_mnist(location):
+    mnist = {}
+    for name in filename[:2]:
+        with gzip.open(location + name[1], 'rb') as f:
+            mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=16).reshape(-1, 28 * 28)
+    for name in filename[-2:]:
+        with gzip.open(location + name[1], 'rb') as f:
+            mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=8)
+    with open(location + "mnist.pkl", 'wb') as f:
+        pickle.dump(mnist, f)
+    print("Save complete.")
+
+
+if __name__ == '__main__':
+    download_mnist(location=PYTHONPATH + '/data/')
+    save_mnist(location=PYTHONPATH + '/data/')
