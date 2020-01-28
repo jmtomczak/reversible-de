1eb6d5df61027a9baee58e9bfeec93e677ebf42e
jmtomczak <jakubmkt@gmail.com>
2020-01-28 12:21:03 +0100

    Fixed mistake with data prepocessing!

--- testbeds/mnist_testbed2.py
+++ testbeds/mnist_testbed2.py
@@ -2,6 +2,8 @@ import os
 import gzip
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 from urllib import request
 from scipy.special import softmax
 
@@ -23,6 +25,7 @@ class MNIST(TestBed):
     def __init__(self, name='mnist', image_size=(14, 14), batch_size=1000, train_size=5000):
         super().__init__()
         self.name = name
+        self.image_size = image_size
 
         if not(os.path.isfile(PYTHONPATH + '/data/' + 'mnist.pkl')):
             self.download_mnist(location=PYTHONPATH + '/data/')
@@ -34,19 +37,17 @@ class MNIST(TestBed):
         x_test = x_test / 255.
 
         # TRAIN DATA
-        x_train = x_train[0:train_size]
+        x_train = np.reshape(x_train[0:train_size], (train_size, 28, 28))
         self.x_train = np.zeros((x_train.shape[0], image_size[0], image_size[1]))
         for i in range(x_train.shape[0]):
             self.x_train[i] = resize(x_train[i], image_size, anti_aliasing=True)
-        self.x_train = np.reshape(self.x_train, (x_train.shape[0], image_size[0] * image_size[1]))
         self.y_train = y_train[0:train_size]
 
         # TEST DATA
+        x_test = np.reshape(x_test, (x_test.shape[0], 28, 28))
         self.x_test = np.zeros((x_test.shape[0], image_size[0], image_size[1]))
         for i in range(x_test.shape[0]):
             self.x_test[i] = resize(x_test[i], image_size, anti_aliasing=True)
-        self.x_test = np.reshape(self.x_test, (x_test.shape[0], image_size[0] * image_size[1]))
-
         self.y_test = y_test
 
         self.batch_size = batch_size
@@ -77,6 +78,34 @@ class MNIST(TestBed):
         with open(location + "mnist.pkl", 'rb') as f:
             mnist = pickle.load(f)
         return mnist["training_images"], mnist["training_labels"], mnist["test_images"], mnist["test_labels"]
+
+    def visualize(self, mode='train', size_x=5, size_y=5, file_name='mnist_images'):
+        fig = plt.figure(figsize=(size_x, size_y))
+        gs = gridspec.GridSpec(size_x, size_y)
+        gs.update(wspace=0.05, hspace=0.05)
+
+        if mode == 'train':
+            x_sample = self.x_train[0:size_x * size_y]
+        elif mode == 'test':
+            x_sample = self.x_test[0:size_x * size_y]
+        else:
+            raise ValueError('Mode could be train OR test, nothing else!')
+
+        for i, sample in enumerate(x_sample):
+            ax = plt.subplot(gs[i])
+            plt.axis('off')
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
+            ax.set_aspect('equal')
+            sample = np.expand_dims(sample, 0)
+            sample = sample.reshape((1, self.image_size[0], self.image_size[1]))
+            sample = sample.swapaxes(0, 2)
+            sample = sample.swapaxes(0, 1)
+            sample = sample[:, :, 0]
+            plt.imshow(sample, cmap='gray')
+
+        plt.savefig(file_name + '_' + mode + '.pdf', bbox_inches='tight')
+        plt.close(fig)
 
     def objective(self, w, *args):
         hidden_units = args[0]['hidden_units']
@@ -117,7 +146,9 @@ class MNIST(TestBed):
 if __name__ == '__main__':
     import time
 
-    mnist = MNIST()
+    mnist = MNIST(image_size=(14, 14))
+    mnist.visualize(mode='train')
+    mnist.visualize(mode='test')
 
     print(mnist.x_train.shape)
     print(mnist.y_train.shape)
